94ca7937e5d0d07c55d9e66d79a8dc682ceec806
hntrhrt0205 <hunterhart225@gmail.com>
2025-12-03 14:04:29 -0600

    Add files via upload
    added dropdown on hover functionality to the system info

--- app/components/panels.tsx
+++ app/components/panels.tsx
@@ -60,34 +60,47 @@ export const AboutSystemPanel = () => {
     <div className="panel fade-in">
       <h1>About Screen</h1>
       {systemPlatform ? (
-        <div>
-          <div> 
-            <h2>System Information</h2>
+        <div className="info-columns-container">
+          <div className="info-column-syst">
+            <div className="dropdown">
+            <button className="dropbtn"> System Information</button>
+            <div className="dropdown-content">
             <p>Platform: {systemPlatform.platform}</p>
             <p>Architecture: {systemPlatform.arch}</p>
             <p>UpTime (Hrs): {(systemPlatform.upTime / (60 ** 2)).toFixed(2)}</p>
             <p>Total Memory (GB): {(systemPlatform.totalMem / (1024 ** 3)).toFixed(2)}</p>
             <p>Free Memory (GB): {(systemPlatform.freeMem / (1024 ** 3)).toFixed(2)}</p>
+            </div>
+          </div>
           </div>
 
-          <div>
-            <h2>Wi-Fi Information</h2>
+          <div className="info-column">
+            <div className="dropdown">
+              <button className="dropbtn">Wi-Fi Information</button>
+                <div className="dropdown-content">
             <p>Interface: {systemPlatform.networkInfo.iface}</p>
             <p>IP Address: {systemPlatform.networkInfo.address}</p>
             <p>Subnet Mask: {systemPlatform.networkInfo.netmask}</p>
             <p>Family: {systemPlatform.networkInfo.family}</p>
             <p>MAC: {systemPlatform.networkInfo.mac}</p>
+                </div>
+            </div>
           </div>
 
-          <div>
-            <h2>CPU Info</h2>
+          <div className="info-column">
+            <div className="dropdown">
+              <button className="dropbtn">CPU Information</button>
+                <div className="dropdown-content">
                 <p>Processor Type: {systemPlatform.cpuInfo.Name} MHz</p>
                 <p>Core Count: {systemPlatform.cpuInfo.NumberOfCores} MHz</p>
                 <p>Thread Count: {systemPlatform.cpuInfo.NumberOfLogicalProcessors} MHz</p>
                 <p>Max Clock Speed: {systemPlatform.cpuInfo.MaxClockSpeed} MHz</p>
+                </div>
           </div>
-
+          </div>
         </div>
+
+
       ) : (
         <p>Loading system information...</p>
       )}
