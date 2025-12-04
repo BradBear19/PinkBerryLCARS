ec5245f96c0e7c7d55d8cc5e68831ea354ac7d72
BradBear19 <bfking0217@gmail.com>
2025-12-04 15:15:41 -0600

    auto function update

--- loadLCARS.bat
+++ loadLCARS.bat
@@ -3,4 +3,4 @@ REM Navigate to the folder where this script is located
 cd /d "%~dp0"
 
 REM Open a command prompt and run the command
-cmd /k "npm run dev:all"
+cmd /k "npm install && npm run dev:all"
